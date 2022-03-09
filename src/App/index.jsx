2b669b5f6ef53d9d84c4dc0be73864d7df1c6cfd
Devin Chenyang <devin.chenyang@grabtaxi.com>
2022-03-09 16:16:00 +0800

    Slider to show pipelines

---
+++ src/App/index.jsx
@@ -0,0 +1,43 @@
+import { Slider, Avatar, Row, Col } from "antd";
+import moment from "moment";
+
+import "antd/dist/antd.css";
+
+function formatter(value) {
+  return moment.unix(value).format("YYYY-MM-DD HH:mm:SS");
+}
+
+const App = () => {
+  return (
+    <div>
+      {JSON.parse(localStorage.getItem("pipelines.json")).pipelines.map(
+        (pipeline) => (
+          <Row key={pipeline.id}>
+            <Col span={1}>
+              <Avatar src={pipeline.user.avatar_url} />
+            </Col>
+            <Col span={23}>
+              <Slider
+                disabled
+                range={!!pipeline.details.finished_at}
+                min={moment().unix() - 3600 * 6}
+                max={moment().unix()}
+                defaultValue={
+                  pipeline.details.finished_at
+                    ? [
+                        moment(pipeline.created_at).unix(),
+                        moment(pipeline.details.finished_at).unix(),
+                      ]
+                    : moment(pipeline.created_at).unix()
+                }
+                tipFormatter={formatter}
+              />
+            </Col>
+          </Row>
+        )
+      )}
+    </div>
+  );
+};
+
+export default App;
